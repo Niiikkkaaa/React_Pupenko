0ef29e90de89f4e495482cedb9b2372dd896efec
niiikkkaaa <nikkollnikki@gmail.com>
2020-10-04 00:07:11 +0300

    homework2 added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 import Week1 from "./homeworks/homework_1/Week1";
 import Blog from "./homeworks/homework_3/Blog";
+import Week2 from "./homeworks/homework_2/Week2";
 
 class App extends Component {
 
   render() {
     return (
       <>
+        <Week2 />
         <Blog />
       </>
     );
